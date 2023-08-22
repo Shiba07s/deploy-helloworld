e1690af50a7ef632327f3e97f90b7ce02b558855
Ashutosh Samantaray <116368054+Shiba07s@users.noreply.github.com>
2023-08-22 11:37:38 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/helloworld/HelloWorldApplication.java
+++ src/main/java/com/helloworld/HelloWorldApplication.java
@@ -3,13 +3,15 @@ package com.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
 public class HelloWorldApplication {
 	
+	@GetMapping("/message")
 	public String getMessage() {
-		return "welcome to LD Tech";
+		return "welcome to LD Tech ..!!!";
 		
 	}
 	 
