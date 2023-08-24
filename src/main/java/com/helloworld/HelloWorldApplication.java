ce32f3fa59fe647ff61de610329a236144f94490
Ashutosh Samantaray <116368054+Shiba07s@users.noreply.github.com>
2023-08-24 16:51:40 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/helloworld/HelloWorldApplication.java
+++ src/main/java/com/helloworld/HelloWorldApplication.java
@@ -11,8 +11,8 @@ public class HelloWorldApplication {
 	
 	@GetMapping("/message")
 	public String getMessage() {
-		//return "welcome to LD Tech ..!!!";
-		return "https://www.google.com/search?q=welcome+page&rlz=1C1CHBF_enIN1046IN1046&oq=welcome+page&aqs=chrome.0.0i433i512j0i512l9.5223j0j7&sourceid=chrome&ie=UTF-8#vhid=RsuozKMYb_Mj7M&vssid=l";
+		return "welcome to LD Tech ..!!!";
+		//return "https://www.google.com/search?q=welcome+page&rlz=1C1CHBF_enIN1046IN1046&oq=welcome+page&aqs=chrome.0.0i433i512j0i512l9.5223j0j7&sourceid=chrome&ie=UTF-8#vhid=RsuozKMYb_Mj7M&vssid=l";
 	}
 	 
 	public static void main(String[] args) {
